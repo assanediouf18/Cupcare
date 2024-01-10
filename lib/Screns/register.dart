90d48be38a8bea6f06a81122af9a4f647da45c62
adiouf <h8799r@gmail.com>
2024-01-10 11:09:26 +0100

    Added AuthenticationForm Component

--- lib/Screns/register.dart
+++ lib/Screns/register.dart
@@ -1,3 +1,4 @@
+import 'package:cupcare/Components/authentication_form.dart';
 import 'package:cupcare/color_schemes.g.dart';
 import 'package:cupcare/Screns/login.dart';
 import 'package:flutter/material.dart';
@@ -14,114 +15,9 @@ class Register extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset('assets/images/logo_CupCare.png'),
-            _buildForm(context)
+            AuthenticationForm()
           ],
         ),
-      ),
-    );
-  }
-
-  Container _formField(var hintText) {
-    return Container(
-      margin: EdgeInsets.only(left: 40, right: 40),
-      decoration: BoxDecoration(boxShadow: [
-        BoxShadow(
-          color: Colors.black.withOpacity(0.11),
-          blurRadius: 40,
-          spreadRadius: 0.0,
-        )
-      ]),
-      child: TextField(
-        decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            contentPadding: EdgeInsets.all(5),
-            hintText: hintText,
-            hintStyle: TextStyle(
-              color: Colors.grey,
-              fontSize: 14,
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: BorderSide.none,
-            )),
-      ),
-    );
-  }
-
-  Form _buildForm(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          Text(
-            'Prénom',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'NunitoSans',
-                color: Colors.black),
-          ),
-          SizedBox(height: 5),
-          _formField(""),
-          SizedBox(height: 5),
-          Text(
-            'Email',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'NunitoSans',
-                color: Colors.black),
-          ),
-          SizedBox(height: 5),
-          _formField(""),
-          SizedBox(height: 5),
-          Text(
-            'Mot de passe',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'NunitoSans',
-                color: Colors.black),
-          ),
-          SizedBox(height: 5),
-          _formField(""),
-          SizedBox(height: 5),
-          Text(
-            'Confirmer le mot de passe',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'NunitoSans',
-                color: Colors.black),
-          ),
-          SizedBox(height: 5),
-          _formField(""),
-          SizedBox(height: 10),
-          TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Login()),
-                );
-              },
-              child: Text("J'ai déjà un compte, me connecter",
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: 'NunitoSans',
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,
-                      decoration: TextDecoration.underline))),
-          SizedBox(height: 5),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-            child: Text(
-              "S'inscrire",
-              style: TextStyle(
-                  color: Colors.white, fontFamily: 'NunitoSans', fontSize: 20),
-            ),
-          ),
-        ],
       ),
     );
   }
